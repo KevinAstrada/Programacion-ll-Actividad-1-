2a1b66e8c6f1e2a00a2f1d66f817bbeba2c46aaf
Kevin <kevin.agustin.astrada@gmail.com>
2026-04-06 16:27:30 -0300

    proyecto django users/roles con sqlite

---
+++ api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import User, Role
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ['id', 'name', 'description']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'name', 'email', 'role', 'activo']
