82ab1931b3eb26a6f2c0875d7c04c225c9b14ff2
Kevin <kevin.agustin.astrada@gmail.com>
2026-04-05 21:38:36 -0300

    primer commit API REST Users/Roles con Django y DRF

---
+++ mi_proyecto/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='users')
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
